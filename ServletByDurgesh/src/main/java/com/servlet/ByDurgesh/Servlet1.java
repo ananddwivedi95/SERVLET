dc0167665b5a4d2beed4001342c28217a5e0a8d3
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-05-11 09:05:21 +0530

    Add files via upload

---
+++ ServletByDurgesh/src/main/java/com/servlet/ByDurgesh/Servlet1.java
@@ -0,0 +1,30 @@
+package com.servlet.ByDurgesh;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/test")
+public class Servlet1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+	  PrintWriter out=response.getWriter();
+	  out.println("<h1> servlet1 is executed</h1>");	
+	}
+
+}
